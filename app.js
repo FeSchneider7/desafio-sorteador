581628294f4889fdf23ad4e82492fcdcb3396111
Fernando Schneider <fernandomoraes26@gmail.com>
2025-03-27 17:17:48 -0300

    desafio

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-function sortear () {
-    let quantidade = document.getElementById('quantidade').value; 
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
 
-    let numeroSorteado = Math.floor(Math.random() * (ate - de + 1)) + de;
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let numerosSorteados = [];
+    let numeroSorteado; 
+
+    for (let i = 0; i < quantidade; i++){
+        numeroSorteado = obterNumeroAleatorio(de, ate);
+        while (numerosSorteados.includes(numeroSorteado)){
+            numeroSorteado = obterNumeroAleatorio(de, ate);
+        }
+        numerosSorteados.push(numeroSorteado);
+
+    }
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`;
+    
 }
+
+
+function obterNumeroAleatorio(de, ate) {
+    return Math.floor(Math.random() * (ate - de + 1)) + de;
+}
+
+
+
+
